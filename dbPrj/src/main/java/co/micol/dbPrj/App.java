ac2d62fd939b3adb2204e18750d3000189e18de4
admin <admin@YD02-13>
2021-10-07 15:58:32 +0900

    오라클 연동

---
+++ dbPrj/src/main/java/co/micol/dbPrj/App.java
@@ -0,0 +1,16 @@
+package co.micol.dbPrj;
+
+import co.micol.dbPrj.db.DevServiceImpl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       DevServiceImpl dao = new DevServiceImpl();
+       dao.selectList();
+   }
+}
---
+++ dbPrj/src/main/java/co/micol/dbPrj/db/DevServiceImpl.java
@@ -0,0 +1,41 @@
+package co.micol.dbPrj.db;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import co.micol.dbPrj.DataSource;
+
+public class DevServiceImpl extends DataSource {
+	private PreparedStatement psmt;
+	private ResultSet rs;
+	
+//	public List<DevVO> selectList(){
+//		DevVO vo;
+//		List<DevVO> list = new ArrayList<DevVO>();
+//		
+//		return list;
+//	}
+	
+	public void selectList() {
+		String sql="select * from dev";
+		try {
+			psmt=conn.prepareStatement(sql);
+			rs=psmt.executeQuery();
+			while(rs.next()) { // 결과가 존재하면
+				System.out.println(rs.getInt("id"));
+				System.out.println(rs.getString("name"));
+				System.out.println(rs.getString("address"));
+				System.out.println(rs.getInt("age"));
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+	}
+
+	}
+
+
